abb9c553acd8431cbbb670deccdc221de615a45d
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-09-08 20:19:59 +0900

    feature(category-findAll): 모든 카테고리 정보 조회
    - 모든 카테고리 정보를 리스트로 조회

--- src/main/java/com/example/onionstore/domain/category/controller/CategoryController.java
+++ src/main/java/com/example/onionstore/domain/category/controller/CategoryController.java
@@ -6,14 +6,15 @@ import com.example.onionstore.domain.category.service.CategoryService;
 import com.example.onionstore.global.dto.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
