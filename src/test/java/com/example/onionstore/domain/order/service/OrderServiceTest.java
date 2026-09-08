0002db7e4edd0d12d26784a5cf521e1f5c164b61
chungmani <cndaks19@naver.com>
2026-09-09 00:22:59 +0900

    test: 주문 상세 조회 성공 여부

--- src/test/java/com/example/onionstore/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/example/onionstore/domain/order/service/OrderServiceTest.java
@@ -1,19 +1,28 @@
 package com.example.onionstore.domain.order.service;
 
+import com.example.onionstore.domain.category.entity.Category;
+import com.example.onionstore.domain.order.dto.GetOrderItemResponse;
+import com.example.onionstore.domain.order.dto.GetOrderResponse;
 import com.example.onionstore.domain.order.entity.Order;
+import com.example.onionstore.domain.order.entity.OrderItem;
 import com.example.onionstore.domain.order.repository.OrderItemRepository;
 import com.example.onionstore.domain.order.repository.OrderRepository;
+import com.example.onionstore.domain.payment.entity.Payment;
 import com.example.onionstore.domain.payment.repository.PaymentRepository;
+import com.example.onionstore.domain.product.entity.Product;
 import com.example.onionstore.domain.user.entity.Role;
 import com.example.onionstore.domain.user.entity.User;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -36,9 +45,28 @@ class OrderServiceTest {
         User user = new User("test@test.com", "1234", "테스트", "010-0000-0000", Role.CUSTOMER);
         Order order = new Order("1", user, 5000);
 
+        Long orderId = 1L;
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+
+        Payment payment = new Payment(order, 5000);
+        when(paymentRepository.findByOrderId(orderId)).thenReturn(Optional.of(payment));
+
+        Category category = new Category("양파");
+        Product product = new Product(category, "양파즙", "역대급 양파즙", 5000, 10);
+        OrderItem orderItem = new OrderItem(order, product, product.getName(), product.getPrice(), 1);
+        when(orderItemRepository.findAllByOrderId(orderId)).thenReturn(List.of(orderItem));
+
         // when
+        GetOrderResponse result = orderService.getOne(orderId);
 
         // then
+        assertEquals(order.getOrderNumber(), result.orderNumber());
+        assertEquals(5000, result.totalPrice());
+        assertEquals(payment.getStatus(), result.paymentStatus());
+        assertEquals(1, result.orderItems().size());
+
+        GetOrderItemResponse resultItem = result.orderItems().get(0);
+        assertEquals(orderItem.getProduct().getName(), resultItem.productName());
 
     }
 
