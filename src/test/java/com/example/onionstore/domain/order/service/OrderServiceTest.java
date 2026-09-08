823584094e8d434644779b43f07e951e35581b14
chungmani <cndaks19@naver.com>
2026-09-08 20:54:46 +0900

    test: 주문 상세 조회 성공 여부

--- src/test/java/com/example/onionstore/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/example/onionstore/domain/order/service/OrderServiceTest.java
@@ -35,6 +35,7 @@ class OrderServiceTest {
         // given
         User user = new User("test@test.com", "1234", "테스트", "010-0000-0000", Role.CUSTOMER);
         Order order = new Order("1", user, 5000);
+        dddd
 
         // when
 
