537f5542bab2d16b21afc9f561b2ab174fc08007
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-09-08 17:44:16 +0900

    feature(product-add): 상품 엔티티 수정
    - 재고 타입 long으로 변경
    - soft delete 정책에 부합하도록 deleted 필드 추가
    - 생성자 private으로 만들고 정적 팩토리 메서드 추가

--- src/main/java/com/example/onionstore/domain/product/entity/Product.java
+++ src/main/java/com/example/onionstore/domain/product/entity/Product.java
@@ -2,6 +2,8 @@ package com.example.onionstore.domain.product.entity;
 
 import com.example.onionstore.domain.category.entity.Category;
 import com.example.onionstore.global.entity.BaseTimeEntity;
+import com.example.onionstore.global.exception.BusinessException;
+import com.example.onionstore.global.exception.ErrorCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -42,7 +44,7 @@ public class Product extends BaseTimeEntity {
     private long price;
 
     @Column(nullable = false)
-    private int stock;
+    private long stock;
 
     @Column(name = "like_count", nullable = false)
     private long likeCount;
@@ -51,7 +53,10 @@ public class Product extends BaseTimeEntity {
     @Column(nullable = false, length = 20)
     private ProductStatus status;
 
-    public Product(Category category, String name, String description, long price, int stock) {
+    @Column(nullable = false)
+    private boolean deleted;
+
+    private Product(Category category, String name, String description, long price, long stock) {
         this.category = category;
         this.name = name;
         this.description = description;
@@ -59,6 +64,10 @@ public class Product extends BaseTimeEntity {
         this.stock = stock;
         this.likeCount = 0;
         this.status = stock == 0 ? ProductStatus.SOLD_OUT : ProductStatus.SELLING;
+        this.deleted = false;
     }
 
+    public static Product create(Category category, String name, String description, long price, long stock) {
+        return new Product(category, name, description, price, stock);
+    }
 }
