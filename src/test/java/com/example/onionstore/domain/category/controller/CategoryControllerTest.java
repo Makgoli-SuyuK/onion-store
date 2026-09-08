832996dfc84af157cda2ee487701b8a7bf3951c8
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-09-08 19:33:40 +0900

    test(category-add): 카테고리 추가 기능 테스트
    - 해당 카테고리 이름이 이미 존재하는 경우 409 에러 반환
    - service 테스트에서 DB 저장 확인
    - controller에서 입력값 검증 테스트

---
+++ src/test/java/com/example/onionstore/domain/category/controller/CategoryControllerTest.java
@@ -0,0 +1,81 @@
+package com.example.onionstore.domain.category.controller;
+
+import com.example.onionstore.domain.category.dto.CategoryCreateRequest;
+import com.example.onionstore.domain.category.service.CategoryService;
+import com.example.onionstore.global.exception.BusinessException;
+import com.example.onionstore.global.exception.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoryController.class)
+class CategoryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private CategoryService categoryService;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    @DisplayName("POST /categories api - 카테고리 추가 테스트")
+    void 카테고리를_추가할_수_있다() throws Exception {
+        //given
+        CategoryCreateRequest createRequest = new CategoryCreateRequest(
+                "name"
+        );
+
+        //when&then
+        mockMvc.perform(post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(createRequest)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("POST /categories api - 이미 존재하는 카테고리 이름")
+    void 이미_존재하는_카테고리_이름으로_카테고리를_추가하면_409_에러가_발생한다() throws Exception {
+        //given
+        CategoryCreateRequest createRequest = new CategoryCreateRequest(
+                "exists"
+        );
+
+        willThrow(new BusinessException(ErrorCode.DUPLICATE_CATEGORY))
+                .given(categoryService)
+                .addCategory(createRequest);
+
+        //when&then
+        mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createRequest)))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value(ErrorCode.DUPLICATE_CATEGORY.getMessage()));
+    }
+
+    @Test
+    @DisplayName("POST /categories api - 입력값 검증 실패")
+    void 카테고리_추가_시_이름이_공백일_수_없다() throws Exception {
+        //given
+        CategoryCreateRequest createRequest = new CategoryCreateRequest(
+                ""
+        );
+
+        //when&then
+        mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(ErrorCode.INVALID_INPUT_VALUE.getMessage()));
+    }
+}
