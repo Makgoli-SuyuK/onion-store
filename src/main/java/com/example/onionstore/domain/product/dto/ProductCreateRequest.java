ae0506d691ae69b77204f71ef50e49155614c9ee
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-09-08 17:45:26 +0900

    add(product-add): dto 추가
    - 상품 생성 api에 대한 dto ProductCreateRequest 추가

---
+++ src/main/java/com/example/onionstore/domain/product/dto/ProductCreateRequest.java
@@ -0,0 +1,22 @@
+package com.example.onionstore.domain.product.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record ProductCreateRequest(
+        @NotBlank
+        String category,
+
+        @NotBlank
+        String productName,
+
+        @NotBlank
+        String description,
+
+        @Min(1)
+        long price,
+
+        @Min(1)
+        long stock
+) {
+}
