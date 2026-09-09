e8290247cece3398ad36166330e17a28f6142fb7
trex1004 <trexs@gmail.com>
2026-09-10 03:22:51 +0900

    feat: 포트원 설정값 바인딩 클래스 추가

---
+++ src/main/java/com/example/onionstore/infra/config/PortOneProperties.java
@@ -0,0 +1,18 @@
+package com.example.onionstore.infra.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "portone")
+public class PortOneProperties {
+    private String baseUrl;
+    private String apiSecret;
+    private String storeId;
+    private String channelKey;
+
+}
