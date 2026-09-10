d9227e087b33282763a153ae7e0c62e8e774d4a6
taeheon <psw4497@gamil.com>
2026-09-10 19:58:08 +0900

    feat: 채팅방 상태 변경 API 추가

---
+++ src/main/java/com/example/onionstore/domain/chat/controller/ChatAdminController.java
@@ -0,0 +1,33 @@
+package com.example.onionstore.domain.chat.controller;
+
+import com.example.onionstore.domain.chat.dto.request.ChatRoomStatusUpdateRequest;
+import com.example.onionstore.domain.chat.dto.response.ChatRoomStatusUpdateResponse;
+import com.example.onionstore.domain.chat.service.ChatRoomService;
+import com.example.onionstore.global.dto.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/chats")
+public class ChatAdminController {
+
+    private final ChatRoomService chatRoomService;
+
+    @PatchMapping("/rooms/{rooomId}/status")
+    public ResponseEntity<ApiResponse<ChatRoomStatusUpdateResponse>> updateStatus(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable Long roomId,
+            @Valid @RequestBody ChatRoomStatusUpdateRequest request
+            ){
+        Long userId = Long.valueOf(jwt.getSubject());
+        ChatRoomStatusUpdateResponse response =
+                chatRoomService.updateStatus(userId, roomId, request.getStatus());
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+}
+
