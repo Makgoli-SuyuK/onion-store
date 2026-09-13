9763658bb1369a8bd3dce715ddc7cbb7568b8328
trex1004 <trexs@gmail.com>
2026-09-13 23:10:47 +0900

    feat: 웹훅 이벤트 조회 실패 에러 코드 추가

--- src/main/java/com/example/onionstore/global/exception/ErrorCode.java
+++ src/main/java/com/example/onionstore/global/exception/ErrorCode.java
@@ -74,7 +74,7 @@ public enum ErrorCode {
 
     // webhook
     WEBHOOK_VERIFICATION_FAILED(HttpStatus.BAD_REQUEST, "WEBHOOK_001", "웹훅 서명검증에 실패했습니다"),
-
+    WEBHOOK_EVENT_NOT_FOUND(HttpStatus.NOT_FOUND, "WEBHOOK_002","웹훅 이벤트를 찾을 수 없습니다."),
     // 이벤트
     EVENT_NOT_STARTED(HttpStatus.CONFLICT, "EVENT_001", "아직 타임세일 시작 전입니다."),
     EVENT_ENDED(HttpStatus.CONFLICT, "EVENT_002", "타임세일이 종료되었습니다."),
